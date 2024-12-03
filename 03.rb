d399a9879fdca6409df3d7bbd1df28cbe6e6c48b
Floris Zeven <floris.zeven@axini.com>
2024-12-03 18:05:41 +0100

    Day 3 part 1

--- 03.rb
+++ 03.rb
@@ -1,23 +1,51 @@
 require_relative 'aoc'
 
-class Day3 < AoCExerciseSolver
-  def initialize(*args); super; end
+class MullItOverSolver < AoCExerciseSolver
+  attr_accessor :raw_lines
+
+  MUL_REGEX = /mul\((\d+),(\d+)\)/
+  DO_OR_DONT_REGEX = /(?=do\(\)|don't\(\))/
+
+  def initialize(*args)
+    @raw_lines = []
+    super
+  end
 
   def preprocess
     parse_lines do |line|
-      
+      @raw_lines << line
     end
   end
-  
+
+  def sum_muls_in_str(str)
+    str.scan(MUL_REGEX).sum do |mul|
+      left, right = mul.map(&:to_i)
+      left * right
+    end
+  end
+
   def solve_part_1
-    
+    raw_lines.sum do |line|
+      sum_muls_in_str(line)
+    end
   end
 
   def solve_part_2
-  
+    raw_lines.sum do |line|
+      dos_or_donts = line.split(DO_OR_DONT_REGEX)
+
+      valid = dos_or_donts.select! do |do_or_dont|
+        !do_or_dont.start_with?("don't()")
+      end
+
+      valid.sum do |str|
+        sum_muls_in_str(str)
+      end
+    end
   end
 end
 
-Day3.new(__FILE__.sub('.rb', '_input.txt'))
-Day3.preprocess
-Day3.solve
+# PART 2: 106266128 TOO HIGH
+solver = MullItOverSolver.new(__FILE__.sub('.rb', '_input.txt'))
+solver.preprocess
+solver.solve
