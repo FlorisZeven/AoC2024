8d11a062888149b9191aa22edc67d9f10e035412
Floris Zeven <floris.zeven@axini.com>
2024-12-06 13:36:19 +0100

    Day 6 Part 1 Cleanup

--- 06.rb
+++ 06.rb
@@ -3,6 +3,9 @@ require_relative 'aoc'
 class GuardGallivantSolver < AoCExerciseSolver
   attr_accessor :lab_grid
 
+  GUARD_CHAR = '^'
+  OBSTRUCTION_CHAR = '#'
+
   def initialize(*args)
     @lab_grid = []
     super
@@ -15,8 +18,8 @@ class GuardGallivantSolver < AoCExerciseSolver
   end
 
   def initial_guard_position
-    row = @lab_grid.index {|row| row.include?('^') }
-    col = @lab_grid[row].index {|char| char == '^'}
+    row = @lab_grid.index {|row| row.include?(GUARD_CHAR) }
+    col = @lab_grid[row].index {|char| char == GUARD_CHAR}
 
     [row, col]
   end
@@ -27,10 +30,14 @@ class GuardGallivantSolver < AoCExerciseSolver
   end
 
   def obstruction?(row, col)
-    @lab_grid[row][col] == '#'
+    @lab_grid[row][col] == OBSTRUCTION_CHAR
   end
 
-  def move_guard(row, col, direction)
+  # Find the next guard position
+  # @return [<Integer, Integer, Symbol, Boolean>] The new row and column of the
+  #   guard, its current direction, and whether it is out of bounds.
+  #   If the guard is out of bounds, don't move the guard.
+  def next_guard_position(row, col, direction)
     case direction
     when :up
       return [row, col, direction, true] if out_of_bounds?(row - 1, col)
@@ -55,12 +62,12 @@ class GuardGallivantSolver < AoCExerciseSolver
     [row, col, direction, false]
   end
 
-  def count_positions(grid)
+  def count_marked_positions(grid)
     grid.sum {|row| row.count {|cell| cell == true}}
   end
 
   def solve_part_1
-    # Initialize grid with dimensions of lab grid, all with value 'false'
+    # Initialize grid with dimensions of lab grid, all with value 'false'.
     marked_grid = Array.new(@lab_grid.length){Array.new(@lab_grid[0].length, false)}
 
     out_of_bounds = false
@@ -69,10 +76,10 @@ class GuardGallivantSolver < AoCExerciseSolver
 
     until out_of_bounds
       marked_grid[guard_row][guard_col] = true
-      guard_row, guard_col, direction, out_of_bounds = move_guard(guard_row, guard_col, direction)
+      guard_row, guard_col, direction, out_of_bounds = next_guard_position(guard_row, guard_col, direction)
     end
 
-    count_positions(marked_grid)
+    count_marked_positions(marked_grid)
   end
 
   def solve_part_2
