2f26de10b907b9bb622d642fbe6645a635f635c4
Floris Zeven <floris.zeven@axini.com>
2024-12-13 16:45:48 +0100

    Day 13 - Cleanup

--- 13/13.rb
+++ 13/13.rb
@@ -10,11 +10,10 @@ class ClawContraptionSolver < AoCExerciseSolver
   end
 
   def preprocess
-    i = 0
-    a_x = a_y = b_x = b_y = sum_x = sum_y = 0
+    a_x = a_y = b_x = b_y = sum_x = sum_y = nil
 
-    File.readlines(@input_file).each do |line|
-      # Since the input is structured evenly, we can use the line index to determine its contents
+    # Since the input is structured evenly, we can use the line index to determine its contents
+    File.readlines(@input_file).each_with_index do |line, i|
       remainder = i % 4
       case remainder
       when 0 # Button A
@@ -23,36 +22,35 @@ class ClawContraptionSolver < AoCExerciseSolver
         b_x, b_y = line.scan(/\d+/).map(&:to_i)
       when 2 # Prize
         sum_x, sum_y = line.scan(/\d+/).map(&:to_i)
-      when 3 # Empty line
+      when 3 # Empty line means end of current machine
         @claw_machines << ClawMachine.new(a_x, a_y, b_x, b_y, sum_x, sum_y)
       end
-      i += 1
     end
   end
 
   # @param [ClawMachine] m The machine to test
-  def min_tokens(m)
+  def tokens_for_prize(m)
     # We have two expressions
     #  - a_x + a_presses + b_x + b_presses = sum_x
     #  - a_y + a_presses + b_y + b_presses = sum_y
     #
     # Two unknowns, so:
     #  - Rewrite both expressions to a_presses = ...
-    #  - Set them equal to each other to have b_presses as remaining unknown
+    #  - Set them equal to each other to have b_presses as remaining unknown, then solve
     #  - Once b_presses is known, solve a_presses
     b_presses = (m.a_x * m.sum_y - m.a_y * m.sum_x).to_f / (m.a_x * m.b_y - m.a_y * m.b_x)
     a_presses = (m.sum_x - m.b_x * b_presses).to_f / m.a_x
 
-    # No real solution if a fraction
+    # There is no proper solution if any number of presses is a fraction
     return 0 if b_presses.to_i != b_presses || a_presses.to_i != a_presses
 
     # Calculate tokens
-    3 * a_presses + b_presses
+    (3 * a_presses + b_presses).to_i
   end
 
   def solve_part_1
     @claw_machines.sum do |claw_machine|
-      min_tokens(claw_machine)
+      tokens_for_prize(claw_machine)
     end
   end
 
@@ -64,7 +62,7 @@ class ClawContraptionSolver < AoCExerciseSolver
     end
 
     adjusted_claw_machines.sum do |claw_machine|
-      min_tokens(claw_machine)
+      tokens_for_prize(claw_machine)
     end
   end
 end
