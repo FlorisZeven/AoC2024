aaf24d3419772870dc547171e74497ae569bd185
Floris Zeven <floris.zeven@axini.com>
2024-12-17 21:59:36 +0100

    17 - Clean

--- 17/17.rb
+++ 17/17.rb
@@ -4,38 +4,64 @@ require 'set'
 
 class ChronospatialComputerSolver < AoCExerciseSolver
   def initialize(*args)
-    @input = {}
+    @registers = {}
     super
   end
 
   def preprocess
     parse_lines do |line|
       if line.include?('Register A')
-        @input['a'] = line.scan(/\d+/).first.to_i
+        @registers['a'] = line.scan(/\d+/).first.to_i
       elsif line.include?('Register B')
-        @input['b'] = line.scan(/\d+/).first.to_i
+        @registers['b'] = line.scan(/\d+/).first.to_i
       elsif line.include?('Register C')
-        @input['c'] = line.scan(/\d+/).first.to_i
+        @registers['c'] = line.scan(/\d+/).first.to_i
       elsif line.include?('Program')
-        @input['program'] = line.scan(/\d+/).join
+        @program = line.scan(/\d+/).join
       end
     end
   end
 
   def solve_part_1
-    computer = ChronoSpatialComputer.new(@input['a'], @input['b'], @input['c'])
-    computer.perform_program(@input['program'])
+    computer = ChronoSpatialComputer.new(@registers['a'], @registers['b'], @registers['c'])
+    result = computer.execute_program(@program)
+    result.chars.join(',')
   end
 
   def solve_part_2
+    @computer = ChronoSpatialComputer.new(0, 0, 0)
 
+    @part_2_options = []
+    # Browse the unique integers for the first bit of the register
+    (0..7).each do |i|
+      find_register_for_program(i, 1)
+    end
+    @part_2_options.min
+  end
+
+  # Find register values that are substrings of the end result
+  def find_register_for_program(register_a, bits_processed)
+    @computer.reset_computer(register_a, 0, 0)
+    result = @computer.execute_program(@program)
+
+    @part_2_options << register_a if result == @program
+
+    # No need to process
+    return if bits_processed == @program.length || !@program.end_with?(result)
+
+    # Because the results overlap so far, and the processor is 3-bit,
+    # we can determine whether 3-bit-shifting left (times 2^3) with unique integer
+    # values (+i) still yields correct results
+    (0..7).each do |i|
+      next_register_a = (register_a * 8) + i
+      find_register_for_program(next_register_a, bits_processed + 1)
+    end
   end
 end
 
 class ChronoSpatialComputer
   attr_accessor :register_a, :register_b, :register_c
-  attr_accessor :instruction_pointer
-  attr_reader :output
+  attr_reader :instruction_pointer, :output
 
   OPCODES = {
     0 => 'adv', 1 => 'bxl', 2 => 'bst', 3 => 'jnz', 4 => 'bxc',
@@ -43,26 +69,25 @@ class ChronoSpatialComputer
   }.freeze
 
   def initialize(a, b, c)
-    @register_a = a
-    @register_b = b
-    @register_c = c
-    @instruction_pointer = nil
-    @output = nil
+    reset_computer(a, b, c)
   end
 
   # @param [<<Integer, Integer>>] instructions a list of instructions
-  def perform_program(program)
-    @instruction_pointer = 0
-    @output = ''
-    pp_computer
+  def execute_program(program)
     until program[@instruction_pointer].nil? || program[@instruction_pointer + 1].nil?
       opcode = program[@instruction_pointer].to_i
       operand = program[@instruction_pointer + 1].to_i
-      pp_next_instruction(program)
       instruct(opcode, operand)
-      pp_computer
     end
-    @output.chars.join(',')
+    @output
+  end
+
+  def reset_computer(a, b, c)
+    @register_a = a
+    @register_b = b
+    @register_c = c
+    @output = ''
+    @instruction_pointer = 0
   end
 
   private
@@ -86,7 +111,7 @@ class ChronoSpatialComputer
 
   def value_for_combo_operand(operand)
     case operand
-    when 0,1,2,3 then operand
+    when 0, 1, 2, 3 then operand
     when 4 then @register_a
     when 5 then @register_b
     when 6 then @register_c
@@ -97,9 +122,7 @@ class ChronoSpatialComputer
 
   # Shared between adv bdv cdv
   def division(operand)
-    numerator = @register_a
-    denominator = 2**value_for_combo_operand(operand)
-    (numerator / denominator).to_i
+    (@register_a / 2**value_for_combo_operand(operand)).to_i
   end
 
   # 0
@@ -109,7 +132,7 @@ class ChronoSpatialComputer
 
   # 1
   def bxl(operand)
-    @register_b = (@register_b ^ operand).to_s(2).to_i
+    @register_b ^= operand
   end
 
   # 2
@@ -119,22 +142,17 @@ class ChronoSpatialComputer
 
   # 3
   def jnz(operand)
-    if @register_a.zero?
-      @instruction_pointer += 2
-    else
-      @instruction_pointer = operand
-    end
+    @register_a.zero? ? @instruction_pointer += 2 : @instruction_pointer = operand
   end
 
   # 4
   def bxc(_operand)
-    @register_b = (@register_b ^ @register_c).to_s(2).to_i
+    @register_b ^= @register_c
   end
 
   # 5
   def out(operand)
-    output = value_for_combo_operand(operand) % 8
-    @output += output.to_s
+    @output += (value_for_combo_operand(operand) % 8).to_s
   end
 
   # 6
@@ -146,7 +164,6 @@ class ChronoSpatialComputer
   def cdv(operand)
     @register_c = division(operand)
   end
-
 end
 
 test_solver = ChronospatialComputerSolver.new(__FILE__.sub('.rb', '_test.txt'))
